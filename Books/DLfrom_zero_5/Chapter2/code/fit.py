6de62686c3f264d00c2d09c00d47aab618e059a3
morixxfoxdata <morixx.datascience@gmail.com>
2024-05-20 13:03:37 +0900

    2.2 done

--- Books/DLfrom_zero_5/Chapter2/code/fit.py
+++ Books/DLfrom_zero_5/Chapter2/code/fit.py
@@ -1,8 +1,8 @@
 # from Chapter1.code.norm_dist import normal
 # from hist import export_xs
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
 def export_xs():
     path = os.path.join(os.path.dirname(__file__), 'height.txt')
     xs = np.loadtxt(path)
@@ -14,7 +14,7 @@ def normal(x, mu=0, sigma=1):
 def fit_plot():
     xs = export_xs()
     mu = np.mean(xs)
-    sigma = np.mean(xs)
+    sigma = np.std(xs)
     x = np.linspace(150, 190, 1000)
     y = normal(x, mu, sigma)
     plt.hist(xs, bins='auto', density=True)
@@ -24,4 +24,5 @@ def fit_plot():
     plt.show()
 
 if __name__ == '__main__':
+    # print(fit_plot())
     fit_plot()
