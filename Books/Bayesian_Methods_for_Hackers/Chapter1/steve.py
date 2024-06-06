1337be1ea636132e70e8123d7d431984a7ce66ee
morixxfoxdata <morixx.datascience@gmail.com>
2024-06-06 11:23:01 +0900

    1.2 done

---
+++ Books/Bayesian_Methods_for_Hackers/Chapter1/steve.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# figsize(12.5, 4)
+
+colors = ["#348ABD", "#A60628"]
+prior = [1/21., 20/21.]
+posterior = [0.087, 1 - 0.087]
+plt.bar([0, .7], prior, alpha=0.70, width=0.25,
+        color=colors[0], label="prior distribution",
+        lw=3, edgecolor="#348ABD")
+
+plt.bar([0 + 0.25, .7 + 0.25], posterior, alpha=0.70, width=0.25,
+        color=colors[1], label="posterior distribution", lw=3, edgecolor="#A60628")
+
+plt.xticks([0.20, 0.95], ["Librarian", "Farmer"])
+plt.ylabel("Probability")
+plt.legend(loc="upper left")
+plt.title("Prior and posterior probabilities of Steve's occupation")
+plt.show()
